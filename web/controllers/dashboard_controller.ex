b70f37b05212514b51740723eeea7389a2d69676
Brian Mullis <brian.mullis09@gmail.com>
2018-09-29 15:45:31 -0700

    User can now update First Name, Last Name, Email. Major updates to overall design.

--- web/controllers/dashboard_controller.ex
+++ web/controllers/dashboard_controller.ex
@@ -2,6 +2,9 @@ defmodule Storyvue.DashboardController do
   use Storyvue.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    current_user_id = Plug.Conn.get_session(conn, :current_user)
+    story_query = from s in Storyvue.Story, where: s.user_id == ^current_user_id
+    stories = Repo.all(story_query)
+    render conn, "index.html", stories: stories
   end
 end
